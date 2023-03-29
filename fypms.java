57490bccbb2e9b885a83a87f004d021b50a813fc
NaqDoo <129270719+NaqDoo@users.noreply.github.com>
2023-03-29 18:08:46 +0800

    Update fypms.java

--- fypms.java
+++ fypms.java
@@ -7,13 +7,71 @@ import java.util.Scanner;
             int choice;
             String userID;
             String userPassword = password;
-            
-            System.out.println("Enter your ID: ");
-           
             Scanner sc = new Scanner(System.in);
             
-            while(userPassword = password){
-                System.out.println("(1) 
+            
+            
+            do{
+                System.out.println("(1) Login");
+                System.out.println("(2) Change Password");
+                System.out.println("(3) Exit");
+                choice = sc.nextInt();
+                
+                switch(choice){
+                    case 1: 
+                        login();
+                        break;
+                    case 2:
+                        changePassword();
+                        break;
+                        
+                       
+                        
+                                
+                          
+                            
+                        
+                            
+                            
+                        
+      
+            } while (choice != 3);
+          
+       public login(){                                      
+           System.out.println("Enter your ID: ");
+                        userID = sc.nextStr();
+                        System.out.println("Enter password: ");
+                        inputPassword = sc.nextStr();
+                        if (inputPassword != userPassword){
+                            break;
+                        }
+                        else {
+                            FYP_Queries;
+                        }
+         }
+                                              
+         public changePassword(){
+             passwordMatching;
+                    System.out.println("Enter current password: ");
+                    oldPassword = sc.nextStr();
+                    if (oldPassword == userPassword){
+                        System.out.println("Enter new password: ");
+                        newPassword1 = sc.nextStr();
+                        System.out.println("Confirm your new password: ");
+                        newPassword2 = sc.nextStr();
+                        if (newPassword1 == newPassword2){
+                            userPassword = newPassword1;
+                        }
+                        else{
+                            System.out.println("The passwords do not match. Please try again.");
+                            continue passwordMatching;
+                        }
+                    }
+                    else{
+                        System.out.println("
+            
+         
+   
         }
     }
 
