c727a6b6c50ae2a63f867d06edfe91068db62fc0
fathamsters <129270699+fathamsters@users.noreply.github.com>
2023-04-03 09:20:53 +0800

    Update fypms.java

--- fypms.java
+++ fypms.java
@@ -1,77 +1,88 @@
-package assignment_2002;
-
+import java.util.ArrayList;
 import java.util.Scanner;
 
-    public class fypms{
-        public static void main (String[] args){
-            int choice;
-            String userID;
-            String userPassword = password;
-            Scanner sc = new Scanner(System.in);
-            
-            
-            
-            do{
-                System.out.println("(1) Login");
-                System.out.println("(2) Change Password");
-                System.out.println("(3) Exit");
-                choice = sc.nextInt();
-                
-                switch(choice){
-                    case 1: 
-                        login();
-                        break;
-                    case 2:
-                        changePassword();
-                        break;
-                        
-                       
-                        
+public class fypms{
+    public String userID;
+    public String inputPassword;
+    public String userPassword = "password";
+    public Scanner sc = new Scanner(System.in);
+
+    public boolean login(){    
+        ArrayList<String[]> studentInfo = new ArrayList<>();
+        studentInfo.add(new String[]{"YCHERN", "password"});
+        studentInfo.add(new String[]{"KOH1", "password"});
+        studentInfo.add(new String[]{"BR015", "password"});
+        studentInfo.add(new String[]{"CT113", "password"});
+        studentInfo.add(new String[]{"YCN019", "password"});
+        studentInfo.add(new String[]{"DL007", "password"});
+        studentInfo.add(new String[]{"DON84", "password"});
+        studentInfo.add(new String[]{"ELI34", "password"});
+        studentInfo.add(new String[]{"LE51", "password"});
+        studentInfo.add(new String[]{"SL22", "password"});
+        studentInfo.add(new String[]{"AKY013", "password"});  
+        System.out.println("Enter your ID: ");
+                    userID = sc.nextLine();
+                    System.out.println("Enter password: ");
+                    inputPassword = sc.nextLine();
+                    boolean foundStudent = false;
+                    for (String[] student : studentInfo) {
+                        if (student[0].equals(userID)) {
+                            if (!inputPassword.equals(student[1])) {
+                                System.out.println("Wrong password!");
+                                break;
+                            } else {
+                                foundStudent = true;
                                 
-                          
-                            
-                        
-                            
-                            
-                        
-      
-            } while (choice != 3);
-          
-       public login(){                                      
-           System.out.println("Enter your ID: ");
-                        userID = sc.nextStr();
-                        System.out.println("Enter password: ");
-                        inputPassword = sc.nextStr();
-                        if (inputPassword != userPassword){
-                            break;
-                        }
-                        else {
-                            FYP_Queries;
-                        }
-         }
-                                              
-         public changePassword(){
-             passwordMatching;
-                    System.out.println("Enter current password: ");
-                    oldPassword = sc.nextStr();
-                    if (oldPassword == userPassword){
-                        System.out.println("Enter new password: ");
-                        newPassword1 = sc.nextStr();
-                        System.out.println("Confirm your new password: ");
-                        newPassword2 = sc.nextStr();
-                        if (newPassword1 == newPassword2){
-                            userPassword = newPassword1;
-                        }
-                        else{
-                            System.out.println("The passwords do not match. Please try again.");
-                            continue passwordMatching;
+                                break;
+                            }
                         }
                     }
-                    else{
-                        System.out.println("
-            
-         
-   
-        }
+                    return foundStudent;
     }
+                                          
+    public void changePassword(){
+        fypms obj = new fypms();
+        boolean foundStudent = obj.login();
+        int x = 0;
+        String oldPassword, newPassword;
+        if (foundStudent == true){
+            do{
+                    System.out.println("Enter current password: ");
+                    oldPassword = sc.nextLine();
+                    if (oldPassword == userPassword){
+                        System.out.println("Enter new password: ");
+                        newPassword = sc.nextLine();
+                        userPassword = newPassword;
+                        break;
+                    }
+                    else{
+                        System.out.println("Wrong password. Try again.");
+                        x++;
+                    }
+                } while (x <= 3 && oldPassword != userPassword);
+            }
+
+    }
+    public static void main(String[] args) {
+        int choice;
+        
+        do{
+            System.out.println("(1) Login");
+            System.out.println("(2) Change Password");
+            System.out.println("(3) Exit");
+            Scanner sc = new Scanner(System.in);
+            choice = sc.nextInt();
+            fypms obj = new fypms();
+            switch(choice){
+                case 1: 
+                    obj.login();
+                    break;
+                case 2:
+                    obj.changePassword();
+                    break;
+            }
+                    
+        } while (choice != 3);
+    }
+}
 
